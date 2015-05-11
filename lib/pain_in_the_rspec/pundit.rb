5f9dfaac24f18fb6017c74cd8697731729857a9f
Gabe Berke-Williams <gabebw@gabebw.com>
2015-05-10 21:06:56 -0400

    Filter out common words in specs for variety

--- lib/pain_in_the_rspec/pundit.rb
+++ lib/pain_in_the_rspec/pundit.rb
@@ -2,16 +2,22 @@ require "girls_just_want_to_have_puns"
 
 module PainInTheRspec
   class Pundit
+    BANNED_WORDS = %w(should can and)
+
     def initialize(original)
       @original = original
     end
 
     def pun
-      GirlsJustWantToHavePuns.pun(original).new_phrase
+      GirlsJustWantToHavePuns.pun(filtered.join(" ")).new_phrase
     end
 
     private
 
     attr_reader :original
+
+    def filtered
+      original.split(" ") - BANNED_WORDS
+    end
   end
 end
--- spec/pain_in_the_rspec/pundit_spec.rb
+++ spec/pain_in_the_rspec/pundit_spec.rb
@@ -5,7 +5,6 @@ describe PainInTheRspec::Pundit do
     it "generates a pun" do
       original = "hello"
       pundit = described_class.new(original)
-      allow(GirlsJustWantToHavePuns).to receive(:pun).and_call_original
 
       expect(pundit.pun).not_to eq original
     end
@@ -13,11 +12,29 @@ describe PainInTheRspec::Pundit do
     it "uses Girls Just Want to Have Puns" do
       original = "hello"
       pundit = described_class.new(original)
-      allow(GirlsJustWantToHavePuns).to receive(:pun).and_call_original
+      stub_pun
 
       pundit.pun
 
       expect(GirlsJustWantToHavePuns).to have_received(:pun)
     end
+
+    context "filtering" do
+      %w(should can and).each do |word|
+        it "filters out '#{word}'" do
+          original = "hello"
+          pundit = described_class.new("#{word} #{original} #{word}")
+          stub_pun
+
+          pundit.pun
+
+          expect(GirlsJustWantToHavePuns).to have_received(:pun).with(original)
+        end
+      end
+    end
+  end
+
+  def stub_pun
+    allow(GirlsJustWantToHavePuns).to receive(:pun).and_call_original
   end
 end
